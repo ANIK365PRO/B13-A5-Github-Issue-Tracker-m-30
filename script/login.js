49b243fa36593f5865818a37f5a9eda25e462d14
Anik Mohanta <mohonto05@gmail.com>
2026-03-08 13:15:52 +0600

    added login js responsive status card

---
+++ script/login.js
@@ -0,0 +1,17 @@
+// console.log('login page connected')
+document.getElementById('loginBtn').addEventListener('click', function(e){
+    e.preventDefault()
+    
+
+    const userName = document.getElementById('userName').value;
+    const userPassword = document.getElementById('userPassword').value
+
+    userName === 'admin'? 
+        (userPassword ==='admin123'? 
+            (window.location.href = `./index.html`): alert('please enter valid user password'))
+    :alert('please enter valid user name')
+
+    // if(userName === 'admin' && userPassword === 'admin123'){
+    //     window.location.href = './index.html'
+    // }
+})
